e604cb579d96599adf1fae7293f33778867331fb
trung1127 <92491678+trung1127@users.noreply.github.com>
2021-12-11 08:13:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ bot.on('login',function(){
 bot.on('time', function(time) {
 	if(nightskip == "true"){
 	if(bot.time.timeOfDay >= 13000){
-	bot.chat('/login afkbot123456')
+	bot.chat('/time set day')
 	}}
     if (connected <1) {
         return;
